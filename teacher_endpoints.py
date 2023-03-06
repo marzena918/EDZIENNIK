509402f7025b5dcb55e7d3227807e06a7f9e6b42
marzenusia <nsnjjs@com.pl>
2023-03-06 09:30:21 +0100

    edit teacher

--- teacher_endpoints.py
+++ teacher_endpoints.py
@@ -1,4 +1,4 @@
-from flask import render_template, Blueprint
+from flask import render_template, Blueprint, request
 
 from teachers import teachers
 
@@ -11,3 +11,26 @@ def index():
 @teacher_blueprint.route('/teacher/get_all_subjects')
 def get_all():
     return teachers.get_all()
+
+@teacher_blueprint.route('/teacher/check_pesel/<pesel>')
+def check_pesel(pesel):
+    return '1' if teachers.is_pesel_exist(pesel) else '0'
+
+@teacher_blueprint.route('/teacher/add', methods= ['POST'])
+def add():
+    data = request.json
+    teachers.add_teacher(data['name'], data['last_name'], data['pesel'], data['subjects'])
+    return ''
+
+@teacher_blueprint.route('/teacher/get_all_teacher')
+def get_all_teachers():
+    return teachers.get_all_teacher()
+
+@teacher_blueprint.route('/teacher/subjectsOfTeacher/<id>')
+def subjects_of_teacher(id):
+    return teachers.subjects_of_teacher(id)
+
+@teacher_blueprint.route('/teacher/delete_subject_for_teacher/<id>/<subject>', methods=['DELETE'])
+def delete_subject_for_teacher(id,subject):
+    teachers.delete_subject_for_teacher(id,subject)
+    return ''
--- teachers.py
+++ teachers.py
@@ -1,7 +1,34 @@
-from student import cursor
+from student import cursor, sqliteConnection
+
 
 class Teacher:
     def get_all(self):
         return cursor.execute("select * from subject").fetchall()
 
+    def is_pesel_exist(self, pesel):
+        return bool(cursor.execute(f"select pesel from teacher where pesel = '{pesel}'").fetchall())
+
+    def add_teacher(self, name, last_name, pesel, sub):
+        cursor.execute(
+            f"insert into teacher (name,last_name,pesel) VALUES('{name}', '{last_name}', '{pesel}')")
+        teacher_id = cursor.lastrowid
+
+        for i in sub:
+            cursor.execute(f"insert into teacher_subject(teacher_id, subject) VALUES ({teacher_id}, '{i}')")
+        sqliteConnection.commit()
+
+    def get_all_teacher(self):
+        return cursor.execute("select * from teacher").fetchall()
+
+    def subjects_of_teacher(self, id):
+        return cursor.execute(f"select subject from teacher_subject where teacher_id = {id}").fetchall()
+        # res2 = result[:]
+        # if not result:
+        #     return ''
+        # else:
+        #     print(res2)
+        #     return res2
+    def delete_subject_for_teacher(self, id,subject):
+        cursor.execute(f"DELETE from teacher_subject where teacher_id = '{id}' and subject = '{subject}'")
+        sqliteConnection.commit()
 teachers = Teacher()
