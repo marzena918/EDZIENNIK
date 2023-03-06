17ab15a3f9cd474a46be8622585576c9ed009cf6
marzenusia <nsnjjs@com.pl>
2023-03-06 09:28:44 +0100

    commented student grades

--- student.py
+++ student.py
@@ -35,12 +35,12 @@ class Student:
     def is_pesel_exist(self, pesel) -> bool:
         return bool(cursor.execute(f"select pesel from student where pesel = '{pesel}'").fetchall())
 
-    def add_grades(self):
-        cursor.execute("insert into oceny(religia, angielski, polski, matematyka, id) VALUES (5,5,5,5,33)")
-        sqliteConnection.commit()
-
-    def get_all_grades(self,id):
-        return cursor.execute(f"select * from oceny where id='{id}'").fetchall()
+    # def add_grades(self):
+    #     cursor.execute("insert into oceny(religia, angielski, polski, matematyka, id) VALUES (5,5,5,5,33)")
+    #     sqliteConnection.commit()
+    #
+    # def get_all_grades(self,id):
+    #     return cursor.execute(f"select * from oceny where id='{id}'").fetchall()
 
     def student_data(self,id):
         return cursor.execute(f"select name, last_name from student where id = '{id}'").fetchall()
