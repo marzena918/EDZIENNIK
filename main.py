ffbbb051b4815d9643aae118cbe27d0c434079de
marzenusia <nsnjjs@com.pl>
2023-03-15 10:18:06 +0100

    cleaning

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from flask import Flask
-from flask import request, render_template
 
 from class_endpoints import class_blueprint
-from student import Student
 from student_endpoints import student_blueprint
 from subject_endpoints import subject_blueprint
 from teacher_endpoints import teacher_blueprint
