b48ce6f552e49fcd8841554b930d21a856d4df48
marzenusia <nsnjjs@com.pl>
2023-03-27 13:02:55 +0200

    added marks to menu

--- mark_endopoints.py
+++ mark_endopoints.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template
 
-marks_blueprint = Blueprint('student_blueprint', __name__)
+marks_blueprint = Blueprint('marks_blueprint', __name__)
 
 @marks_blueprint.route('/marks')
 def index():
-    return render_template('mark.html')
+    return render_template('marks.html')
--- static/menu.js
+++ static/menu.js
@@ -1,4 +1,4 @@
-function createMenu(parentObject){
+function createMenu(parentObject) {
     const classs = document.createElement("a");
     classs.href = "/class";
     classs.innerHTML = 'class';
@@ -8,11 +8,15 @@ function createMenu(parentObject){
     const subjects = document.createElement("a");
     subjects.href = "/subject";
     subjects.innerHTML = 'subjects';
-       const index = document.createElement("a");
+    const index = document.createElement("a");
     index.href = "/student";
     index.innerHTML = 'students';
+    const marks = document.createElement("a");
+    marks.href = "/marks";
+    marks.innerHTML = 'marks';
     parentObject.appendChild(classs);
     parentObject.appendChild(nauczyciele);
     parentObject.appendChild(subjects);
     parentObject.appendChild(index);
+    parentObject.appendChild(marks);
 }
