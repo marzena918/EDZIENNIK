fec64dbb9c28667f220489daf8933dc55b1137ae
marzenusia <nsnjjs@com.pl>
2023-03-27 16:55:58 +0200

    backend added findDegre and Add degre

--- degre.py
+++ degre.py
@@ -1,1 +1,11 @@
 from student import cursor, sqliteConnection
+
+class Degre:
+    def findDegreForStudentWithSubject(self,name,last_name,subject):
+        cursor.execute(f"select mark from marks where subject = '{subject}' and student = (select id from student where name ='{name}' and last_name ='{last_name}')").fetchall()
+
+    def add_degre(self,student_id,mark,notes, subject):
+        cursor.execute(f"insert into marks(subject, mark, student, notes) values ('{subject}','{mark}', {student_id},'{notes}')")
+        sqliteConnection.commit()
+
+degre = Degre()
