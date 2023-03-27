45256adad099f695cf735474bf94ef22690f8097
marzenusia <nsnjjs@com.pl>
2023-03-27 14:06:12 +0200

    added two starting selections

--- mark_endopoints.py
+++ mark_endopoints.py
@@ -1,7 +1,17 @@
 from flask import Blueprint, render_template
 
+from marks import marks
+
 marks_blueprint = Blueprint('marks_blueprint', __name__)
 
 @marks_blueprint.route('/marks')
 def index():
     return render_template('marks.html')
+()
+@marks_blueprint.route('/getAllSubjects')
+def get_all_subjects():
+    return marks.get_all_subjects()
+
+@marks_blueprint.route('getAllClasses')
+def getAllClasses():
+    return marks.getAllClasses()
--- marks.py
+++ marks.py
@@ -1,1 +1,10 @@
 from student import cursor, sqliteConnection
+
+
+class Mark:
+    def get_all_subjects(self):
+        return cursor.execute("select * from subject").fetchall()
+
+    def getAllClasses(self):
+        return cursor.execute("select * from class").fetchall()
+marks = Mark()
