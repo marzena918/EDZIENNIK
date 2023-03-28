e5a760c4fb481553bb542479f3dff17c87d1966a
marzenusia <nsnjjs@com.pl>
2023-03-28 10:07:52 +0200

    added grades with notes

--- degre_endpoints.py
+++ degre_endpoints.py
@@ -1,3 +1,12 @@
-from flask import Blueprint
+from flask import Blueprint, request
+
+from degre import degre
 
 degre_blueprint = Blueprint('degre_blueprint', __name__)
+
+
+@degre_blueprint.route('/degre/addDegre', methods=['POST'])
+def add_degre():
+    data = request.json
+    degre.add_degre(data['id'], data['mark'],data['notes'], data['subject'])
+    return ''
