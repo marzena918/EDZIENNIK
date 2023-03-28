d9c1e55d4a9ad39c6aacf62dc2902c99718634ff
marzenusia <nsnjjs@com.pl>
2023-03-28 11:34:58 +0200

    save grades

--- degre_endpoints.py
+++ degre_endpoints.py
@@ -8,5 +8,10 @@ degre_blueprint = Blueprint('degre_blueprint', __name__)
 @degre_blueprint.route('/degre/addDegre', methods=['POST'])
 def add_degre():
     data = request.json
-    degre.add_degre(data['id'], data['mark'],data['notes'], data['subject'])
+    degre.add_degre(data['id'], data['mark'], data['notes'], data['subject'])
     return ''
+
+
+@degre_blueprint.route('/degre/degreWithThisSubjectForThisStudent/<subject>/<student_id>')
+def degreWithThisSubjectForThisStudent(subject, student_id):
+    return degre.findDegreForStudentWithSubject(subject, student_id)
