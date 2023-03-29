a15ade39e195cb32f42bec256c0637c0a36bd20c
marzenusia <nsnjjs@com.pl>
2023-03-29 11:27:55 +0200

    fixed typo id

--- degre_endpoints.py
+++ degre_endpoints.py
@@ -8,7 +8,7 @@ degre_blueprint = Blueprint('degre_blueprint', __name__)
 @degre_blueprint.route('/degre/addDegre', methods=['POST'])
 def add_degre():
     data = request.json
-    degre.add_degre(data['id'], data['mark'], data['notes'], data['subject'])
+    degre.add_degre(data['studentId'], data['mark'], data['notes'], data['subject'])
     return ''
 
 
