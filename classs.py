5e98896362988e97964312f1dfe47c35994972a4
marzenusia <nsnjjs@com.pl>
2023-05-09 11:48:08 +0200

    select of classes

--- classs.py
+++ classs.py
@@ -16,7 +16,7 @@ cursor = sqliteConnection.cursor()
 
 class Classs:
     def get_all(self):
-        return cursor.execute("select * from class join teacher on class.tutor = teacher.id").fetchall()
+        return cursor.execute("select class.name as name_class,class.id, teacher.name, teacher.last_name from class join teacher on class.tutor = teacher.id").fetchall()
 
 
     def get_all_teacher(self):
--- lesson_plan_endpoints.py
+++ lesson_plan_endpoints.py
@@ -1,7 +1,13 @@
 from flask import Blueprint, render_template
 
+from classs import classs
+
 lesson_plan_blueprint = Blueprint('lesson_plan_blueprint', __name__)
 
 @lesson_plan_blueprint.route('/lessonPlan')
 def index():
     return render_template('lesson_plan.html')
+
+@lesson_plan_blueprint.route('/get_all_names_class')
+def get_all_name_class():
+    return classs.get_all()
