9740e02b684a6ca364b812b00dd5abca7fa30d56
marzenusia <nsnjjs@com.pl>
2023-05-12 15:18:42 +0200

    save attendance from database

--- attendance.py
+++ attendance.py
@@ -1,3 +1,5 @@
+import datetime
+
 from student import cursor, sqliteConnection
 class Attendance:
     def get_sub_hour(self,day,classes):
@@ -7,7 +9,6 @@ class Attendance:
         for i in is_full:
             if i['subject'] != '0':
                 res.append(i)
-                print(i['subject'])
         return res
 
     def get_all_hour(self):
@@ -15,6 +16,14 @@ class Attendance:
         res = cursor.execute(f" select id, from_hour, until_hours from configurations_hours").fetchall()
         for i in res:
             di[i['id']]="-".join([i['from_hour'],i['until_hours']])
-        print(di)
         return di
+
+    def save(self,student, hour, subject, classes, date, inputPresent, inputLate):
+        cursor.execute(f"delete from attendance where student= {student} and hour = {hour}")
+
+        cursor.execute(f"insert into attendance (day, hour, class, student, lesson, present, late) VALUES ({'10-10-1999'}, {hour}, "
+                       f"{classes}, {student}, '{subject}', {bool(inputPresent)}, {bool(inputLate)})")
+
+        sqliteConnection.commit()
+
 attendance = Attendance()
--- attendance_enpoints.py
+++ attendance_enpoints.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 from attendance import attendance
 
@@ -15,3 +15,11 @@ def get_sub_hour(day, classes):
 @attendance_blueprint.route('/attendance/getAllHour')
 def get_all_hour():
     return attendance.get_all_hour()
+
+@attendance_blueprint.route('/attendance/saveAttendance', methods=['POST'])
+
+def save_attendance():
+    body = request.json
+    attendance.save(body['student_id'], body['hour'], body['subject'], body['classes'], body['date'],
+                    body['inputPresent'], body['inputLate'])
+    return ''
