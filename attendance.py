3c7ab653866c2f22d79808590920d48d7e813c7a
marzenusia <nsnjjs@com.pl>
2023-05-12 12:19:24 +0200

    readinreading subjects and hours per day and class

--- attendance.py
+++ attendance.py
@@ -1,1 +1,12 @@
 from student import cursor, sqliteConnection
+class Attendance:
+    def get_sub_hour(self,day,classes):
+        res = []
+        is_full= cursor.execute(f"select hour_id, subject from lesson_plan where day='{day}' and "
+                                f"classes_id = {classes}").fetchall()
+        for i in is_full:
+            if i['hour_id'] != 0:
+                res.append(i)
+        print(res)
+        return res
+attendance = Attendance()
--- attendance_enpoints.py
+++ attendance_enpoints.py
@@ -1,7 +1,13 @@
 from flask import Blueprint, render_template
 
+from attendance import attendance
+
 attendance_blueprint = Blueprint('attendance_blueprint', __name__)
 
 @attendance_blueprint.route('/attendance')
 def init():
     return render_template("attendance.html")
+
+@attendance_blueprint.route('/attendance/getDayAndClasses/<day>/<classes>')
+def get_sub_hour(day, classes):
+    return attendance.get_sub_hour(day,classes)
