bcf2bdffde205074fab6b3e744fc0ff705e3aa3d
marzenusia <nsnjjs@com.pl>
2023-12-28 20:54:46 +0100

    insert null instead of 0

--- lesson_plan.py
+++ lesson_plan.py
@@ -4,22 +4,12 @@ from student import cursor, sqliteConnection
 class LessonPlan:
     def save(self, clases_id, data):
         cursor.execute(f"delete  from lesson_plan where classes_id = {clases_id}")
-
-
         days = {'monday': 'poniedziałek', 'tuesday': 'wtorek', 'wednesday':'środa', 'thursday': 'czwartek', 'friday': 'piątek'}
         for day, v in data.items():
             for hour, sub_teach in v.items():
-
-                # cursor.execute(f"insert into lesson_plan (day, classes_id, hour_id) VALUES ('{day}', {clases_id}, '{hour}'")
-                # # if hour:
-                # #     cursor.execute(f"insert into lesson_plan (hour_id) VALUES ({hour}")
-                # if sub_teach['subject']:
-                #     cursor.execute(f"insert into lesson_plan (subject) VALUES ('{sub_teach['subject']}'")
-                # if sub_teach['teacher']:
-                #     cursor.execute(f"insert into lesson_plan (teacher) VALUES ({sub_teach['teacher']}")
                 cursor.execute(f"insert into lesson_plan (day, hour_id, subject, teacher, classes_id) VALUES "
-                               f"('{days[day]}', {hour}, '{sub_teach['subject'] if sub_teach['subject'] else '0'}', "
-                               f"{sub_teach['teacher'] if sub_teach['teacher'] else 0}, {clases_id})")
+                               f"('{days[day]}', {hour}, '{sub_teach.get('subject')}', {sub_teach.get('teacher')},"
+                               f" {clases_id})")
         sqliteConnection.commit()
 
     def get_plan(self, class_id):
